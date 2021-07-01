d4d7558b08c65e4de16b074321d0d493879b2ac4
dimaalabsi8 <>
2021-07-01 13:21:38 +0000

    read.js

--- app.js
+++ app.js
@@ -1,17 +1,1 @@
-var name = prompt("what's your name?")
-alert("welcome to my website " +  name)
-
-
-
-
-var favourite = prompt ("what about your favourite movie?")
-
-console.log( favourite)
-
- if(favourite == "me before you"){document.write( "<p>"+ 
-"great, let's enjoy with our favourite movie " + "</p>")}
-
-
-
-
-else { alert("sorry this website is not about your favourite one")}
+var 
